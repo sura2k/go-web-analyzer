f88a0b3b48f94003ef81a40c58d4a3729e5efa4b
sura2k <sura.btaccel@gamil.com>
2025-02-06 16:10:52 +0530

    Heading tags analyzer implementation

--- services/analyzers/html_heading_tags_analyzer.go
+++ services/analyzers/html_heading_tags_analyzer.go
@@ -4,8 +4,26 @@ import (
 	"golang.org/x/net/html"
 )
 
-// GetPageTitle returns the <title> text in the given page body
-func GetHeadingTags(htmlDoc *html.Node) string {
-	//TODO
-	return "DEMO"
+// GetHeadingTags returns a map of all heading levels availabes and counts for the given htmlDoc
+func GetHeadingTags(htmlDoc *html.Node) map[string]int {
+	headings := make(map[string]int)
+
+	// Inner recursive functions MUST be declared first since it calls itself within the same context
+	var traverse func(*html.Node)
+
+	// Traverse through the node tree and locate all H tags, and add to map with incrementing the count
+	traverse = func(node *html.Node) {
+		if node.Type == html.ElementNode {
+			switch node.Data {
+			case "h1", "h2", "h3", "h4", "h5", "h6":
+				headings[node.Data] = headings[node.Data] + 1
+			}
+		}
+		for elem := node.FirstChild; elem != nil; elem = elem.NextSibling {
+			traverse(elem)
+		}
+	}
+
+	traverse(htmlDoc)
+	return headings
 }
