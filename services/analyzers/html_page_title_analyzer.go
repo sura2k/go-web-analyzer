01afabda2f9bfcb88f0c19b316159d590e5aff1e
sura2k <sura.btaccel@gamil.com>
2025-02-06 15:41:06 +0530

    Page Title analyzer implementation

--- services/analyzers/html_page_title_analyzer.go
+++ services/analyzers/html_page_title_analyzer.go
@@ -1,11 +1,40 @@
 package analyzers
 
 import (
+	"strings"
+
 	"golang.org/x/net/html"
 )
 
-// GetPageTitle returns the <title> text in the given page body
+// GetPageTitle returns the page title i.e. value of <title> for the given htmlDoc
 func GetPageTitle(htmlDoc *html.Node) string {
-	//TODO
-	return "DEMO"
+	head := findHeadTag(htmlDoc)
+	return findTitle(head)
+}
+
+// Finds the <head> element in the HTML document
+func findHeadTag(htmlDoc *html.Node) *html.Node {
+	if htmlDoc.Type == html.ElementNode && htmlDoc.Data == "head" {
+		return htmlDoc
+	}
+	for elem := htmlDoc.FirstChild; elem != nil; elem = elem.NextSibling {
+		head := findHeadTag(elem)
+		if head != nil {
+			return head
+		}
+	}
+	return nil
+}
+
+// Finds the <title> element inside the <head> tag and return the value
+func findTitle(head *html.Node) string {
+	if head == nil {
+		return ""
+	}
+	for elem := head.FirstChild; elem != nil; elem = elem.NextSibling {
+		if elem.Type == html.ElementNode && elem.Data == "title" && elem.FirstChild != nil {
+			return strings.TrimSpace(elem.FirstChild.Data)
+		}
+	}
+	return ""
 }
